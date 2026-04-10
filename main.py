2014827537e6742ae9e91f60d8656ab96e458084
inkmas <wenyuan_zhao@outlook.com>
2026-04-10 22:13:25 +0800

    feat(main): successfully run app

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-from threading import Thread
+import time
+from multiprocessing import Process
 
 import uvicorn
 from PySide6.QtCore import QUrl
+from PySide6.QtWebEngineCore import QWebEngineSettings
 from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtWidgets import QApplication
 from PySide6.QtWidgets import QMainWindow
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from settings import WEBPAGE_PATH
+from src.routers.cookie import cookie
+from src.routers.download import download
+from src.utils.port import is_api_ready
 
 fastapi_app = FastAPI(title="Downloader API")
+
+fastapi_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许所有来源（开发环境）
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许所有 HTTP 方法
+    allow_headers=["*"],  # 允许所有 Header
+)
+
+fastapi_app.include_router(download.router)
+fastapi_app.include_router(cookie.router)
 
 
 class MainWindow(QMainWindow):
@@ -22,19 +39,34 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.browser)
 
 
-def run_fastapi():
-    uvicorn.run(fastapi_app, host="127.0.0.1", port=8000)
+def run_fastapi(port: int):
+    uvicorn.run(fastapi_app, host="127.0.0.1", port=port)
 
 
 def main():
-    fastapi_thread = Thread(target=run_fastapi, daemon=True)
-    fastapi_thread.start()
+    port = 56000
+    fastapi_process = Process(target=run_fastapi, args=(port,))
+    fastapi_process.start()
+
+    for _ in range(100):
+        if is_api_ready(port):
+            time.sleep(2)
+            break
+        time.sleep(0.1)
 
     pyside_app = QApplication([])
     window = MainWindow()
+    settings = window.browser.settings()
+    settings.setAttribute(QWebEngineSettings.WebAttribute.LocalContentCanAccessRemoteUrls, True)
+    settings.setAttribute(QWebEngineSettings.WebAttribute.LocalContentCanAccessFileUrls, True)
     window.showMaximized()
     pyside_app.exec()
 
+    # uvicorn.run(
+    #     "main:fastapi_app",
+    #     host="127.0.0.1",
+    #     port=56000,
+    # )
 
 if __name__ == '__main__':
     main()
---
+++ src/utils/port.py
@@ -0,0 +1,13 @@
+import socket
+
+
+def is_api_ready(port: int):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex(('127.0.0.1', port)) == 0
+
+
+def find_available_port() -> int:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('127.0.0.1', 0))
+        port = s.getsockname()[1]
+        return port
